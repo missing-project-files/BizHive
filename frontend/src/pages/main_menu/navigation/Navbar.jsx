95b07403a4534407deb69bcb34a49d482314ad32
FETTZOR <emilir96@protonmail.com>
2022-10-31 20:33:56 +0200

    fontend/initial version of a header

--- frontend/src/pages/main_menu/navigation/Navbar.jsx
+++ frontend/src/pages/main_menu/navigation/Navbar.jsx
@@ -1,49 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FaBars } from "react-icons/fa";
-import "../../../index.css";
+import "../../../assets/styles/main_menu/header.css";
 import "./navbar_data";
 
-import logo from "./logo.svg";
+import logo from "./../../../assets/images/main_menu/logo.svg";
+import { links, pages } from "./navbar_data";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
   return (
     <nav>
-      <div className={"nav-center"}>
-        <div className={"nav-header"}>
-          <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
-            <li>
-              <a href="option1"></a>
-            </li>
-            <li>
-              <a href="option2"></a>
-            </li>
-            <li>
-              <a href="option3"></a>
-            </li>
-            <li>
-              <a href="option4"></a>
-            </li>
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
           </ul>
         </div>
         <ul className="page-icons">
-          <li>
-            <a href="option1"></a>
-          </li>
-          <li>
-            <a href="option2"></a>
-          </li>
-          <li>
-            <a href="option3"></a>
-          </li>
-          <li>
-            <a href="option4"></a>
-          </li>
+          {pages.map((pageIcon) => {
+            const { id, url, icon } = pageIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
