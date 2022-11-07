fb7cec6958ab2f12b29c6001339eb44c5c689913
FETTZOR <emilir96@protonmail.com>
2022-11-07 22:09:24 +0200

    frontend/landing page in progress

--- frontend/src/pages/landing/Landing.jsx
+++ frontend/src/pages/landing/Landing.jsx
@@ -1,20 +1,31 @@
-import logo from "../../assets/images/landing/logo.svg";
-import main from "../../assets/images/landing/main.png";
+import logo from "../../assets/images/landing/logo.png";
+import main from "../../assets/images/landing/coolText.png";
+import "../../assets/styles/landing_styles/landing.css";
 const Landing = () => {
   return (
     <main>
       <nav>
-        <img src={logo} alt="brett statistics logo" className="landingLogo" />
+        <img src={main} alt="brett statistics logo" className="landingLogo" />
       </nav>
       <div className="landingContainerPage">
         {/* info */}
         <div className="landingInfo">
-          <img
-            src={main}
-            alt="landing main image"
-            className="landingMainImage"
-          />
+          <h1>
+            Statistics
+            <span> Analysis</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <button className="landingLoginBtn">Login/Register</button>
         </div>
+        <img src={logo} alt="landing main image" className="landingMainImage" />
       </div>
     </main>
   );
