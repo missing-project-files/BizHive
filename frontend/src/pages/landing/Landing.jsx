70abc9406b5e3cc58a3b7825d0546628c1cac229
FETTZOR <emilir96@protonmail.com>
2022-11-08 22:37:42 +0200

    frontend/styled components test

--- frontend/src/pages/landing/Landing.jsx
+++ frontend/src/pages/landing/Landing.jsx
@@ -1,9 +1,18 @@
 import main_image from "../../assets/images/landing/logo.png";
 import logo_header from "../../assets/images/landing/logo-no-background.svg";
 import "../../assets/styles/landing_styles/landing.css";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: red;
+  color: white;
+  font-size: 2rem;
+`;
+
 const Landing = () => {
   return (
     <main>
+      <Button>Test</Button>
       <nav>
         <img
           src={logo_header}
