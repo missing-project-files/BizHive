98b7b70512828fcf785ed3adab703e85a19732a6
FETTZOR <emilir96@protonmail.com>
2022-11-11 22:56:11 +0200

    frontend/all resolutions support

--- frontend/src/pages/landing/Landing.jsx
+++ frontend/src/pages/landing/Landing.jsx
@@ -5,23 +5,26 @@ import styled from "styled-components";
 
 const Wrapper = styled.main`
   nav {
-    margin-left: 450px;
     height: var(--nav-height);
     display: flex;
-    align-items: center;
-    min-width: 2500px;
   }
-
+  .landingLogo {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+  }
   .landingContainerPage {
     min-height: calc(100vh - var(--nav-height));
     display: grid;
     align-items: center;
     margin-top: -3rem;
-    margin-left: calc(50% - 700px);
+    margin-left: calc(50% - 600px);
   }
 
   h1 {
     font-weight: 700;
+    margin-left: 15%;
     span {
       color: var(--primary-500);
     }
@@ -36,9 +39,85 @@ const Wrapper = styled.main`
   .landingMainImage {
     display: none;
   }
-  @media (min-width: 1420px) {
+  @media (max-width: 389px) {
     .landingContainerPage {
-      grid-template-columns: 650px 1800px;
+      margin: auto;
+      min-height: auto;
+    }
+    h1 {
+      min-height: auto;
+      font-size: 1.052rem;
+      span {
+        display: none;
+      }
+    }
+    p {
+      display: none;
+    }
+    .landingLoginBtn {
+      margin-left: 0;
+      margin-top: 0;
+      margin-right: 0;
+      align-items: flex-start;
+    }
+  }
+  @media (max-width: 247px) {
+    .landingLoginBtn {
+      margin: -20%;
+      padding: 3px 8px;
+      font-size: 10px;
+      line-height: 25px;
+      text-transform: uppercase;
+      font-family: "Montserrat", sans-serif;
+      font-weight: 200;
+    }
+    h1 {
+      margin-left: -5px;
+    }
+  }
+  @media (max-width: 755px) and (min-width: 389px) {
+    .landingContainerPage {
+      margin: auto;
+      min-height: auto;
+    }
+    h1 {
+      min-height: auto;
+      margin: auto;
+      width: 100%;
+      font-weight: 100;
+      span {
+        display: none;
+      }
+    }
+    p {
+      display: none;
+    }
+    .landingLoginBtn {
+      margin: 5%;
+      align-items: center;
+    }
+  }
+  @media (max-width: 1199px) {
+    .landingContainerPage {
+      margin-left: auto;
+      margin-right: auto;
+      width: 50%;
+      align-items: center;
+    }
+    h1 {
+      font-weight: 700;
+      span {
+        color: var(--primary-500);
+      }
+    }
+    .landingInfo {
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
+  @media (min-width: 1200px) {
+    .landingContainerPage {
+      grid-template-columns: 600px 1500px;
       column-gap: 3rem;
     }
     .landingMainImage {
