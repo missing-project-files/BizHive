d40ef152346b9b552a8569a5f0d024ecc9b655b6
Berserkeras <emilir96@protonmail.com>
2022-11-17 22:38:14 +0200

    frontend/applied router to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Landing from "./pages/landing/Landing";
+import Error from "./pages/error/ErrorPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <>
-      <Landing></Landing>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
