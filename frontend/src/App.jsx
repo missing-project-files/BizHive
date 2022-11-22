189057ce5a9ebf165b7e13253ee1bb434df081dd
Berserkeras <emilir96@protonmail.com>
2022-11-22 22:30:52 +0200

    frontend/tied router element to new register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Landing from "./pages/landing/Landing";
+import Register from "./pages/register/Register";
 import Error from "./pages/error/ErrorPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="register" element={<Register />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
