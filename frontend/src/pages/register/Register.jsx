c6434264c8a27a49875067c7cb94c512b2de51a7
Berserkeras <emilir96@protonmail.com>
2022-11-22 23:20:40 +0200

    frontend/temporary disabled eslint due active development

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,4 +1,25 @@
+/* eslint-disable */
+import { useState } from "react";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 const Register = () => {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
     <div>
       <h1>register page</h1>
