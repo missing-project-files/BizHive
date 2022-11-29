3c805070bc3cdb87ca337d99836cb1bd3bc196e4
Berserkeras <emilir96@protonmail.com>
2022-11-29 22:09:17 +0200

    frontend/toast container style fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Route path="register" element={<Register />} />
         <Route path="*" element={<Error />} />
       </Routes>
-      <ToastContainer position="top-center" />
+      <ToastContainer />
     </BrowserRouter>
   );
 }
